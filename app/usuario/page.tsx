2779cf1780175c16d7b909c3e24725baaa8d4bac
Matheus Viana <matheuscviana@ufsj.edu.br>
2026-04-14 13:07:37 -0300

    inicio2

--- app/usuario/page.tsx
+++ app/usuario/page.tsx
@@ -1,8 +1,19 @@
+//import { JSX } from "react";
+
 export default function Pagina() {
+
+  const paragrafos = [];
+  //const v = [1, 2, 3];  
+  //const paragrafos = v.map((i) => <p>{i}</p>);
+
+  for (let i = 0; i < 3; i++) {
+    paragrafos.push(<p key={i}>{i}</p>);
+  }
 
   return (
     <>
       <h1>MInha Página</h1>
+      {paragrafos}
       <p>dssd</p>
     </>
   );
