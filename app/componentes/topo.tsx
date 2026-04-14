088119d07cf1e35a78e2c9536ef78cbb92e3b082
Matheus Viana <matheuscviana@ufsj.edu.br>
2026-04-14 12:46:25 -0300

    inicio

---
+++ app/componentes/topo.tsx
@@ -0,0 +1,7 @@
+export default function Topo() {
+  return (
+    <div className="bg-amber-950">
+      <h1 className="text-center text-amber-600">Título da Minha Página</h1>
+    </div>
+  );
+}
---
+++ app/usuario/page.tsx
@@ -0,0 +1,10 @@
+export default function Pagina() {
+
+  return (
+    <>
+      <h1>MInha Página</h1>
+      <p>dssd</p>
+    </>
+  );
+
+}
